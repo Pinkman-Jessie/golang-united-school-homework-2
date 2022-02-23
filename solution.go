babef5d530137aa5aa328e362cae3f9730ca4605
Vitalii Kharaman <vitalii_kharaman@epam.com>
2022-02-23 14:11:12 +0300

    f

--- solution.go
+++ solution.go
@@ -26,7 +26,7 @@ func CalcSquare(sideLen float64, sidesNum customtype) (res float64) {
 
 	if sidesNum == SidesTriangle {
 
-		res = sideLen * sideLen
+		res = (sideLen * sideLen) * math.Sqrt(3)/2
 	} else if sidesNum == SidesSquare {
 		res = sideLen * sideLen
 	} else if sidesNum == SidesCircle {
